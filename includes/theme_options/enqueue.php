835873e0e75feff0680123f8e888eaabfa131abc
Ciprian Turcu <turcuciprian2.gmail.com>
2014-06-09 22:00:38 +0300

    v 0.0.0.8 Started from scratch, made the basis of a non-dropdown menu

--- includes/theme_options/enqueue.php
+++ includes/theme_options/enqueue.php
@@ -7,7 +7,9 @@ function citrix_enqueue_scripts() {
     global $post;
     //Javascript
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap', 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js', array(), '1.0.0', true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '1.0.0', true);
+    wp_enqueue_script('respond', get_template_directory_uri() . '/js/respond.min.js', array(), '1.0.0', true);
+    wp_enqueue_script('shiv', 'http://html5shiv.googlecode.com/svn/trunk/html5.js', array(), '1.0.0', false);
 
     wp_enqueue_script('cleanscript-core', get_template_directory_uri() . '/js/tcs_core.js', array(), '1.0.0', true);
     wp_enqueue_script('main-script', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true);
@@ -17,7 +19,7 @@ function citrix_enqueue_scripts() {
     }
 
 //    Bootstrap Style
-    wp_enqueue_style('bootstrap-style', 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css');
 //    Google fonts
     wp_enqueue_style('google-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700,800|Open+Sans+Condensed:300|Dosis:200,300,400,500,600,700,800');
 //    Font awesome icons style
